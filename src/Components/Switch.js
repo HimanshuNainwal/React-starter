5338b7e02cbdafe765ce213d3597c355c05ac883
Himanshu Nainwal <himanshunainwal@Himanshus-MacBook-Air.local>
2024-05-24 21:25:02 +0530

    dynamic component

--- src/Components/Switch.js
+++ src/Components/Switch.js
@@ -1,22 +1,54 @@
 import React, { useEffect, useState } from "react";
-import "./switch.css"
+import "./switch.css";
+import Card from "./Card";
 
 function Switch(props) {
   const { data } = props;
 
-  const [activeName,setActiveName] = useState(null )
+  const [activeName, setActiveName] = useState(null);
+  const [cardData, setCardData] = useState(null);
 
+  const handleSetCurrentData = (data) => {
+    setActiveName(data.name);
+    setCardData(data)
+  };
 
-  console.log('activeName',activeName);
+ 
+
   return (
     <div>
       <ul className="listHolder">
-   
-      {data.map((singleData) => {
-        return <li> <button onClick={() => setActiveName(singleData.name)} className={`switchName ${activeName == singleData.name ? "active"  :""}`} > {singleData.name} </button> </li>;
-      })}
+        {data?.map((singleData, index) => {
+          return (
+            <li key={singleData.name}>
+              <button
+                onClick={() => handleSetCurrentData(singleData)}
+                className={`switchName ${
+                  activeName == singleData.name ? "active" : ""
+                }`}
+              >
+                {singleData.name}
+              </button>
 
+              {/* {activeName == singleData.name && ( */}
+              {/* <Card
+                  header={singleData.heading}
+                  content={singleData.content}
+                  code={singleData.code}
+                /> */}
+              {/* )} */}
+            </li>
+          );
+        })}
+
+        <li>Clear</li>
       </ul>
+
+     {cardData ? <Card
+        header={cardData.heading}
+        content={cardData.content}
+        code={cardData.code}
+      /> : <p>Please select a topic.</p>}
     </div>
   );
 }
