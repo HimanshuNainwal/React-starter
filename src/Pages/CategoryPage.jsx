f0d498dc1e98b04ea138dfeefae9ab4adc3dc15b
Himanshu Nainwal <himanshunainwal@Himanshus-MacBook-Air.local>
2024-06-29 20:33:33 +0530

    filter design added

--- src/Pages/CategoryPage.jsx
+++ src/Pages/CategoryPage.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { BASE_URL } from "../../lib/constans";
 import { Triangle } from "react-loader-spinner";
+import SingleFilterComponent from "../components/SingleFilterComponent";
 
 function CategoryPage() {
   const { category } = useParams();
@@ -10,17 +11,19 @@ function CategoryPage() {
   const [CurrentPage, setCurrentPage] = useState(1);
   const [isPageAvailable, setIsPageAvailable] = useState(true);
 
+  const [appliedFilters,setAppliedFilters] = useState({})
+
   const loaderRef = useRef(null);
 
   const [productData, setProductData] = useState([]);
+  const [filtersData, setFiltersData] = useState([]);
 
   const handleScroll = (e) => {
     const topScroll = window.scrollY; // 0 - 20000 -4000
     const loaderScroll = loaderRef?.current?.offsetTop; // 2000 - 4000
 
-
-    // uiLoader it will wait for the api call 
-    // isPageAvailable for when there is next page 
+    // uiLoader it will wait for the api call
+    // isPageAvailable for when there is next page
     if (topScroll > loaderScroll - 1000 && !uiLoader && isPageAvailable) {
       getProductList(category, CurrentPage + 1);
     }
@@ -47,13 +50,15 @@ function CategoryPage() {
       // }&filter=${filter || ""}
       if (response?.status == 200) {
         if (response?.data?.result) {
-          // [1,2,3,4,5,6,7,8,9]
-
+          if (response?.data?.result?.filters?.length) {
+            setFiltersData(response?.data?.result?.filters);
+          }
           if (response?.data?.result?.products?.length) {
             setProductData((state) => [
               ...state,
               ...response?.data?.result?.products,
             ]);
+
             setCurrentPage(page || 1);
           } else {
             setIsPageAvailable(false);
@@ -66,15 +71,36 @@ function CategoryPage() {
     }
   };
 
+  const handleFilterChange = (code,value ) => {
+    const currentFilter = appliedFilters
+
+      if(currentFilter[code]){
+          currentFilter[code] = [...currentFilter[code],value]
+      }else {
+
+          currentFilter[code] = [value]
+
+      }
+
+
+
+      setAppliedFilters(currentFilter)
+
+
+
+
+  }
+
+  console.log('appliedFilters',appliedFilters);
+
   useEffect(() => {
-    setIsPageAvailable(true)
-    setProductData([])
-    window.scrollTo(0,0)
+    setIsPageAvailable(true);
+    setProductData([]);
+    window.scrollTo(0, 0);
     getProductList(category);
   }, [category]);
 
   useEffect(() => {
-
     if (isPageAvailable) {
       window.addEventListener("scroll", handleScroll);
     }
@@ -89,7 +115,21 @@ function CategoryPage() {
   return (
     <>
       <div className="flex gap-8">
-        <div className="w-2/12 border "></div>
+        <div className="w-2/12 border ">
+          {filtersData?.length > 0 &&
+            filtersData.map((filter) => {
+              return (
+                <div key={filter.filter_lable}>
+                  <SingleFilterComponent
+                    title={filter.filter_lable}
+                    filterOption={filter?.options}
+                    appliedFilters={appliedFilters}
+                    handleFilterChange={handleFilterChange}
+                  />
+                </div>
+              );
+            })}
+        </div>
         <div className="flex flex-wrap w-9/12">
           {productData?.length > 0 &&
             productData.map((item) => {
---
+++ src/components/SingleFilterComponent.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function SingleFilterComponent({ title, filterOption , appliedFilters , handleFilterChange }) {
+    console.log('appliedFilters',appliedFilters);
+    const [isOpen,setIsOpen] = useState(false)
+  return (
+    <div>
+      <div className="flex justify-between  px-2 font-bold my-2" onClick={() => setIsOpen(state => !state)}>
+        {" "}
+        <p>{title}</p> <p className="text-xl cursor-pointer">{isOpen?"-" :"+"}</p>{" "}
+      </div>
+
+      {isOpen && <div className="max-h-52 overflow-scroll border  py-2	">
+        {filterOption?.length > 0 &&
+          filterOption?.map((singleFilter) => {
+            return (
+              <div className="p-1">
+
+                <label htmlFor={singleFilter?.value} className="block">
+                  <input
+                    type="checkbox"
+                    onChange={() => handleFilterChange(singleFilter?.code,singleFilter?.value)}
+                    id={singleFilter?.value}
+                    className="mr-2"
+                  />
+                  {singleFilter?.value}
+                </label>
+              </div>
+            );
+          })}
+      </div>}
+    </div>
+  );
+}
+
+export default SingleFilterComponent;
+
