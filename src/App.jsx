62f4702a67bbce89ce759cf47f1edf89f22b928c
Himanshu Nainwal <himanshunainwal@Himanshus-MacBook-Air.local>
2024-07-10 21:14:19 +0530

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,44 @@
-import { useEffect, useState } from "react";
-import AddProject from "./component/AddProject";
-import NoProject from "./component/NoProject";
+import { useState } from "react";
+import { useFormInput } from "./hooks/useFormInput";
+import { useFetch } from "./hooks/useFetch";
 import axios from "axios";
-import { BASE_URL } from "../lib/constans";
-import ShowProject from "./component/ShowProject";
-import { useSelector } from "react-redux";
 
-function App() {
+const getApiData = async () => {
+  const data = await axios("http://localhost:3000/projects");
+  return data?.data;
+};
+export default function Form() {
 
-  const [showAddProject, setShowAddProject] = useState(false);
-  const [currentProject, setCurrentProject] = useState("");
-
-
-  const projects = useSelector(state => state.project)
+  const { isFetching, fetchedData, setFetchedData, error } = useFetch(getApiData , null);
 
 
-
-
-  const handleRefresh = async () => {
-    setCurrentProject("");
-
-  };
-
+  const firstNameProps = useFormInput("lalalal");
+  const lastNameProps = useFormInput("lalalal");
 
   return (
     <>
-      <main className="h-screen my-8 flex gap-8">
-        <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
-          <h1 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
-            {" "}
-            Your Projects
-          </h1>
+      <label>
+        First name:
+        <input {...firstNameProps} />
+      </label>
+      <label>
+        Last name:
+        <input {...lastNameProps} />
+      </label>
 
-          <button
-            onClick={() => setShowAddProject(true)}
-            className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
-          >
-            + Add Project
-          </button>
+      {fetchedData && fetchedData?.map(el => {
+        return <p> {el.title}</p>
+      })}
 
-          <div className="mt-4">
-            {projects?.projectList?.length > 0 &&
-              projects?.projectList.map((singleProject) => {
-                return (
-                  <p
-                    className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800 text-stone-400"
-                    onClick={() => setCurrentProject(singleProject?.id)}
-                  >
-                    {singleProject.title}
-                  </p>
-                );
-              })}
-          </div>
-        </aside>
-        {/* {!showAddProject ? <NoProject /> :<AddProject />} */}
-        {!showAddProject && !currentProject && (
-          <NoProject onOpen={() => setShowAddProject(true)} />
-        )}
-        {currentProject && !showAddProject && (
-          <div className="w-[35rem] mt-16">
-            <ShowProject id={currentProject} closeProject={handleRefresh} />
-          </div>
-        )}
 
-        {showAddProject && (
-          <AddProject
-            formClose={() => setShowAddProject(false)}
+      <button onClick={() => setFetchedData("")}>Clear Data</button>
+      <p>
+        <b>
 
-          />
-        )}
-      </main>
+
+          Good morning, {firstNameProps?.value} {lastNameProps?.value}.
+        </b>
+      </p>
     </>
   );
 }
-
-export default App;
